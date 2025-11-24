2a931dd4cd3d8b4dfd0109cfdbb15746d38c9382
manju1907 <lella.manjusri@gmail.com>
2025-11-24 04:40:45 +0000

    created apis for update and delete for orders

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
+const orders = require('./orders')
 
 
 // Set the port
@@ -19,6 +20,26 @@ app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
+app.get('/orders', api.listOrders)
+app.post('/orders/', api.createOrder)
+app.put('/orders/:id', async (req, res, next) => {
+  try {
+    const updated = await orders.edit(req.params.id, req.body)
+    res.json(updated)
+  } catch (err) {
+    next(err)
+  }
+})
+app.delete('/orders/:id', async (req, res, next) => {
+  try {
+    await orders.destroy(req.params.id)
+    res.status(204).send()   // no content
+  } catch (err) {
+    next(err)
+  }
+})
+
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
